74792f684f01a5bd7c0cdc7c50662de52d81a617
proxime <kulaknorbert@gmail.com>
2019-10-23 19:00:08 +0200

    update

---
+++ client/src/Components/News/HeadNewsPc.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const url = 'https://images.wallpaperscraft.com/image/road_marking_bridge_123398_1920x1080.jpg';
+
+const HeadNewsPc = () => {
+    return (
+        <div className="head-news-container-pc">
+            <div className="head-news-main">
+                <div className="head-news-main-image-container">
+                    <div className="head-news-main-image" style={{ backgroundImage: `url(${url})` }}></div>
+                </div>
+                <div className="head-news-main-text">
+                    <div className="head-news-main-title">
+                        Domowe sposoby badania jakości powietrza.
+                            </div>
+                    <div className="head-news-main-date">
+                        25.10.2019 12:00
+                        </div>
+                </div>
+            </div>
+            <div className="head-news-secondary-container">
+                <div className="head-news-secondary">
+                    <div className="head-news-secondary-image-container">
+                        <div className="head-news-secondary-image" style={{ backgroundImage: `url(${url})` }}></div>
+                    </div>
+                    <div className="head-news-secondary-text">
+                        <div className="head-news-secondary-title">
+                            Domowe sposoby badania jakości powietrza.
+                                    </div>
+                        <div className="head-news-secondary-date">
+                            25.10.2019 12:00
+                                    </div>
+                    </div>
+                </div>
+                <div className="head-news-secondary">
+                    <div className="head-news-secondary-image-container">
+                        <div className="head-news-secondary-image" style={{ backgroundImage: `url(${url})` }}></div>
+                    </div>
+                    <div className="head-news-secondary-text">
+                        <div className="head-news-secondary-title">
+                            Domowe sposoby badania jakości powietrza.
+                                    </div>
+                        <div className="head-news-secondary-date">
+                            25.10.2019 12:00
+                                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeadNewsPc;
--- client/src/Components/News/News.js
+++ client/src/Components/News/News.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './News.css';
+import HeadNewsPc from './HeadNewsPc';
 
 const url = 'https://images.wallpaperscraft.com/image/road_marking_bridge_123398_1920x1080.jpg';
 
@@ -7,8 +8,9 @@ const News = () => {
     return (
         <div className="container">
             <div className="head-news">
-                <div className="head-news-container">
-                    <div className="head-news-main">
+                <HeadNewsPc />
+                <div className="head-news-mobile">
+                    <div className="head-news-mobile-main">
                         <div className="head-news-main-image-container">
                             <div className="head-news-main-image" style={{ backgroundImage: `url(${url})` }}></div>
                         </div>
@@ -21,8 +23,8 @@ const News = () => {
                         </div>
                         </div>
                     </div>
-                    <div className="head-news-secondary-container">
-                        <div className="head-news-secondary">
+                    <div className="head-news-mobile-secondary-container">
+                        <div className="head-news-mobile-secondary">
                             <div className="head-news-secondary-image-container">
                                 <div className="head-news-secondary-image" style={{ backgroundImage: `url(${url})` }}></div>
                             </div>
@@ -35,7 +37,7 @@ const News = () => {
                                     </div>
                             </div>
                         </div>
-                        <div className="head-news-secondary">
+                        <div className="head-news-mobile-secondary">
                             <div className="head-news-secondary-image-container">
                                 <div className="head-news-secondary-image" style={{ backgroundImage: `url(${url})` }}></div>
                             </div>
